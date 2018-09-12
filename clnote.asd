727c8053d580ec9646bc8bec52955698c4e91a0c
Mukhamed Karanashev <menchauser@gmail.com>
2018-09-12 19:29:51 +0300

    Initial commit: common operations on notes DB

---
+++ clnote.asd
@@ -0,0 +1,9 @@
+(defsystem "clnote"
+  :description "clnote: a simple notes storage"
+  :version "0.1.0"
+  :author "Mukhamed Karanashev <menchauser@gmail.com>"
+  :license "MIT License"
+  :depends-on ("local-time"
+			   "uiop")
+  :components ((:file "package")))
+  
---
+++ package.lisp
@@ -0,0 +1,60 @@
+(defpackage :clnote
+  (:use :common-lisp
+		:local-time
+		:uiop)
+  (:export :main))
+
+(in-package :clnote)
+
+(local-time:enable-read-macros)
+
+
+(defvar *notes* nil)
+
+
+;; constructors
+(defun make-note (tag note timestamp)
+  (list :tag tag
+		:timestamp timestamp
+		:text note))
+
+(defun build-note (tag note)
+  "Construct new note object for current time."
+  (make-note tag note (local-time:now)))
+
+
+;; access
+(defun add-note (note)
+  (push note *notes*))
+
+(defun get-notes (tag)
+  "Get all notes for specific tag."
+  (remove-if-not (lambda (note) (equal tag (getf note :tag))) *notes*))
+
+
+;; output 
+(defun dump-notes (notes)
+  (dolist (note notes)
+	(format t "~{~a:~12t~a~%~}~%" note)))
+
+
+;; save and load
+(defun store-notes (path)
+  "Store notes to given path."
+  (with-open-file (out path
+					   :direction :output
+					   :if-exists :supersede)
+	(pprint *notes* out)))
+
+(defun load-notes (path)
+  "Loads notes from given path."
+  (with-open-file (in path
+					  :direction :input
+					  :if-does-not-exist :error)
+	(setf *notes* (read in))))
+
+
+;; main
+(defun main ()
+  (format t "Hello~%")
+  (format t "~a~%" uiop:*command-line-arguments*))
